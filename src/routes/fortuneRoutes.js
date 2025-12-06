b45e4b1fbcb40afcfc888c89a249ce3d11725d84
Mindia Arabuli <mindia.arabuli@iliauni.edu.ge>
2025-12-06 14:58:24 +0400

    Add Fortune models and enhance server setup
    Introduced empty Fortune and FortuneTemplate model files. Updated fortuneRoutes with comments and a new route. Enhanced server.js with dotenv config, CORS setup, JSON parsing, and MongoDB connection logic.

--- src/routes/fortuneRoutes.js
+++ src/routes/fortuneRoutes.js
@@ -2,10 +2,12 @@ import express from "express";
 
 const fortuneRoutes = express.Router();
 
+// facebook.com/api/fortune/all
 fortuneRoutes.get("/all", (req, res) => {
   res.send("All Information");
 });
 
+// facebook.com/api/fortune/all
 fortuneRoutes.get("/templates", (req, res) => {
   res.send("Fortune infomration");
 });
--- src/server.js
+++ src/server.js
@@ -1,12 +1,41 @@
 import express from "express";
 import fortuneRoutes from "./routes/fortuneRoutes.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 
+dotenv.config();
+
+// მომუშავეთა გუნდი
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+// process.env.myPassword = "123456"
+// ✅ MongoDB connection with try/catch
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+}
+connectDB();
 
 app.get("/home", (request, response) => {
   response.send("your welcome");
 });
 
+// localhost:4000
+// facebook.com/api/fortune
 app.use("/api/fortune", fortuneRoutes);
 
 const PORT = 4000;
